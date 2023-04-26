8c5471251f74d730f9996b1c7547277ecd64c79c
МАРК <markpan07@mail.ru>
2023-04-26 19:24:20 +0300

    Initial commit

---
+++ src/main/java/Exceptions/EmployeeBadNameException.java
@@ -0,0 +1,8 @@
+package Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code= HttpStatus.BAD_REQUEST)
+public class EmployeeBadNameException extends RuntimeException {
+}
